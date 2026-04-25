0e2df3a408e8b60bcbcd5abe8cb66e211738c893
Piotr Greń <piotrek.gren2002@gmail.com>
2026-04-25 17:07:35 +0200

    Lint fix

--- apps/backend/src/core/settings.py
+++ apps/backend/src/core/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 import environ
-from datetime import datetime, timedelta
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
